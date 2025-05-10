012dd5f94ebb3a1ddb1404d32b5407b93647d9c1
superRice <1246333567@qq.com>
2025-05-10 15:59:00 +0800

    fix：优化配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
   base: '/fpsActor/',
+  server: {
+    host: "0.0.0.0",
+  },
   plugins: [
     ghPages({
       branch: 'gh-pages',
